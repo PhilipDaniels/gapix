22d02b7dd0f3965230fba2cd4ef28a69cf5191e9
Philip Daniels <Philip.Daniels1971@gmail.com>
2024-10-20 11:51:39 +0100

    Update run.sh to run against my big testing directory.

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-# Remove all output files.
-rm -vf target/release/*simp*.gpx && rm -vf target/release/*.xlsx
-
 # Build separately so that globbing works in the next command.
 cargo build --release
 
-RUST_LOG=DEBUG target/release/gapix -f --metres=5 --analyse target/release/*.gpx
+RUST_BACKTRACE=1 RUST_LOG=DEBUG target/release/gapix -f --metres=5 --analyse /home/phil/OneDrive/Documents/Cycling/Testing\ GPXs/*.gpx
