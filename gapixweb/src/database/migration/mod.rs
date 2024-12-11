f2a2be3cf1b938eb4ccc8cce6283114e3913abeb
Philip Daniels <Philip.Daniels1971@gmail.com>
2024-12-11 14:58:45 +0000

    Factor out a create_table method

--- gapixweb/src/database/migration/mod.rs
+++ gapixweb/src/database/migration/mod.rs
@@ -1,3 +1,4 @@
+use sea_orm::{EntityTrait, Schema};
 pub use sea_orm_migration::prelude::*;
 
 mod m20241211_01;
@@ -13,4 +14,24 @@ impl MigratorTrait for Migrator {
             Box::new(m20241211_02::Migration),
         ]
     }
+}
+
+trait SchemaManagerExtensions {
+
+}
+
+
+async fn create_table<E>(manager: &SchemaManager<'_>, entity: E) -> Result<(), DbErr>
+where
+    E: EntityTrait,
+{
+    let backend = manager.get_database_backend();
+    let schema = Schema::new(backend);
+    let mut table_create_statement = schema.create_table_from_entity(entity);
+    let table_create_statement = table_create_statement.if_not_exists();
+    let stmt = backend.build(table_create_statement);
+    let conn = manager.get_connection();
+    let _ = conn.execute(stmt).await?;
+
+    Ok(())
 }
--- gapixweb/src/database/mod.rs
+++ gapixweb/src/database/mod.rs
@@ -1,3 +1,6 @@
 pub mod migration;
 pub mod model;
 pub mod conn;
+
+pub use model::file::ActiveModel as ActiveFile;
+pub use model::file::Entity as File;
