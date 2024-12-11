199060e95046d483b956bc763a4bd741fb1d70f6
Philip Daniels <Philip.Daniels1971@gmail.com>
2024-12-11 11:58:37 +0000

    Create a table from my own Entity.

---
+++ gapixweb/src/database/migration/m20241211_02.rs
@@ -0,0 +1,26 @@
+use sea_orm::Schema;
+use sea_orm_migration::prelude::*;
+
+use crate::database::file::Entity as File;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let conn = manager.get_connection();
+        let backend = manager.get_database_backend();
+        let schema = Schema::new(backend);
+        // Doesn't work for defaults?
+        let table_create_statement = schema.create_table_from_entity(File);
+        let _ = conn.execute(backend.build(&table_create_statement)).await?;
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(File).to_owned())
+            .await
+    }
+}
--- gapixweb/src/database/migration/mod.rs
+++ gapixweb/src/database/migration/mod.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20241211_01;
+mod m20241211_02;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20241211_01::Migration)]
+        vec![
+            Box::new(m20241211_01::Migration),
+            Box::new(m20241211_02::Migration),
+        ]
     }
 }
