dbf224258d1e3bf8986d99c31786f1f5e280081b
Philip Daniels <Philip.Daniels1971@gmail.com>
2024-12-12 15:26:27 +0000

    Store file in the db with a hash code.

--- gapixweb/src/api/handlers/mod.rs
+++ gapixweb/src/api/handlers/mod.rs
@@ -1,8 +1,10 @@
 use axum::extract::{Multipart, Path};
 use maud::{html, Markup};
+use sea_orm::{ActiveValue, ActiveModelTrait};
+use sha2::{Digest, Sha256};
 use tracing::info;
 
-use crate::{database::entity, error::ApiResult};
+use crate::{database::{conn::make_connection, entity}, error::ApiResult};
 
 pub async fn get_file(Path(id): Path<i32>) -> ApiResult<Markup> {
     let file = entity::get_file(id).await.unwrap();
@@ -32,15 +34,28 @@ pub fn upload_file_view() -> Markup {
 /// once around the loop. It can also accept a single file.
 pub async fn post_files(mut multipart: Multipart) {
     while let Some(field) = multipart.next_field().await.unwrap() {
-        let name = field.name().unwrap().to_string();
+        let name = field.name().unwrap().to_string(); // "file"
         let file_name = field.file_name().unwrap().to_string();
         let content_type = field.content_type().unwrap().to_string();
         let data = field.bytes().await.unwrap();
+        let hash = Sha256::digest(&data);
 
         info!(
-            "Length of `{name}` (`{file_name}`: `{content_type}`) is {} bytes",
+            "Length of `{name}` (`{file_name}`: `{content_type}`) is {} bytes, hash = {hash:?}",
             data.len()
         );
+
+        // // TEST: Insert an entity.
+        let f = entity::file::ActiveModel {
+            name: ActiveValue::Set(file_name),
+            hash: ActiveValue::Set(format!("{:x?}", hash)),
+            data: ActiveValue::Set(data.to_vec()),
+            ..Default::default()
+        };
+
+        let conn = make_connection().await.unwrap();
+        let res = f.insert(&conn).await.unwrap();
+        info!("Returned Id = {}", res.id);
     }
 
     //Ok(markup)
